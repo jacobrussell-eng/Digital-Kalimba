e3e80adfb847b58bded6ace2e0214e3900f05707
Jacob Russell <8bitjacob@gmail.com>
2024-11-04 18:52:34 +0000

    Kalimba control + textures + backgrounds
    Edited the playNote() to be compatible with keyboard events, styled the instrument, added gif backgrounds, made the positioning scalable

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Digital-Kalimba
  Recreation of a Kalimba played with mouse or keyboard
+
+## References:
+Keyboard Events: [MDN](https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key)\
+Kalimba Soundpack: [FreePats](https://freepats.zenvoid.org/Ethnic/kalimba.html)\
+Background gifs: [LivingStills](https://livingstills.tumblr.com)\
+Metal Texture: [Texture Labs](https://texturelabs.org/textures/metal_269/)\
+Wood Texture: [Google Images](https://images.app.goo.gl/Dq5Q12zpLUNzxFYUA)
--- kalimba.js
+++ kalimba.js
@@ -9,24 +9,78 @@ const notes = {
     7: "wavs/7.wav",
     8: "wavs/8.wav"
 }
+let isKeyPressed = {};
+
+// gif reference:
+const gifs = {
+    1: "gifs/daybreak.gif",
+    2: "gifs/fungus.gif",
+    3: "gifs/pier.gif",
+    4: "gifs/rolling-tide.gif",
+    5: "gifs/shoreline.gif",
+    6: "gifs/sunset.gif",
+    7: "gifs/woods.gif"
+}
 
 // HTML element connect:
 const tabs = document.querySelectorAll(".tab");
 
 // Assign event listeners on loading:
 window.addEventListener("DOMContentLoaded", () => {
+    document.body.style.backgroundImage = "url(" + gifs[Math.floor(Math.random()*7+1)];
     tabs.forEach(tab => {
-        tab.addEventListener('click', (event) => playNote(event.target));
+        tab.addEventListener('click', (event) => playNote(event.target.dataset.value));
     })
 })
 
 // Assign keypress events:
 window.addEventListener("keydown", (event) => {
-    // playNote + event.key, assign a connection, or use switch case ie: switch(event.key) { case "A": xxx break; }
+    // Check if key is currently being held down:
+    if (isKeyPressed[event.key]) { return }
+    isKeyPressed[event.key] = true;
+    switch (event.key.toLowerCase()) {
+        case "s":
+        case "1":
+            playNote(1);
+            break;
+        case "d":
+        case "2":
+            playNote(2);
+            break;
+        case "f":
+        case "3":
+            playNote(3);
+            break;
+        case "g":
+        case "4":
+            playNote(4);
+            break;
+        case "h":
+        case "5":
+            playNote(5);
+            break;
+        case "j":
+        case "6":
+            playNote(6);
+            break;
+        case "k":
+        case "7":
+            playNote(7);
+            break;
+        case "l":
+        case "8":
+            playNote(8);
+            break;
+    }
+})
+
+// Release key:
+window.addEventListener('keyup', (event) => {
+    isKeyPressed[event.key] = false;
 })
 
 // Note playing function:
-function playNote(target) {
-    let newNote = new Audio(notes[target.dataset.value]);
+function playNote(num) {
+    let newNote = new Audio(notes[num]);
     newNote.play()
 }
